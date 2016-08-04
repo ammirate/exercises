55db311595222d43dd3f835536794007a0eda4b2
ammirate <cesarano2607@gmail.com>
2016-08-04 11:36:57 +0200

    Implementato ArrayList

---
+++ StruttureDati/Liste/IArrayList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IArrayList<T> {
+
+    bool isEmpty();
+
+    int size();
+
+    void add(T el);
+
+    void add(T el, int i);
+
+    T set(T el, int i);
+
+    T get(int i);
+
+    T remove(int el);
+
+}
+
